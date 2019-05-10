85e709122ad3bb75dfb05e5503015c445e213f11
Kushal Patel <imkushalpatel@live.com>
2019-05-10 18:54:26 +0530

    department added

---
+++ src/main/java/com/example/demo/entities/Department.java
@@ -0,0 +1,19 @@
+package com.example.demo.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "departments")
+public class Department implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3754695570101220513L;
+	
+	
+
+}
