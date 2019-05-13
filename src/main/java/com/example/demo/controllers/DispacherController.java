c7ca43e78ab8425d7894e5f6b62bdc9eb1f7204e
Kushal Patel <imkushalpatel@live.com>
2019-05-13 17:36:48 +0530

    added endpoints

--- src/main/java/com/example/demo/controllers/DispacherController.java
+++ src/main/java/com/example/demo/controllers/DispacherController.java
@@ -1,7 +1,5 @@
 package com.example.demo.controllers;
 
-import java.util.List;
-
 import javax.ws.rs.QueryParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +8,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.entities.Department;
+import com.example.demo.entities.DepartmentEmployee;
+import com.example.demo.entities.DepartmentManager;
 import com.example.demo.entities.Employee;
+import com.example.demo.entities.Salary;
 import com.example.demo.entities.Title;
+import com.example.demo.services.DepartmentEmployeeService;
+import com.example.demo.services.DepartmentManagerService;
+import com.example.demo.services.DepartmentService;
 import com.example.demo.services.EmployeeService;
+import com.example.demo.services.SalaryService;
 import com.example.demo.services.TitleService;
 
 @RestController
@@ -23,19 +29,48 @@ public class DispacherController {
 	@Autowired
 	private TitleService titleService;
 
-	@RequestMapping(path = "/getAllEmployee", method = RequestMethod.GET)
-	public List<Employee> getAllEmployee() {
-		return employeeService.getAllEmployee();
-	}
+	@Autowired
+	private SalaryService salaryService;
+
+	@Autowired
+	private DepartmentService departmentService;
+
+	@Autowired
+	private DepartmentEmployeeService departmentEmployeeService;
+
+	@Autowired
+	private DepartmentManagerService departmentManagerService;
 
 	@RequestMapping(path = "/getEmployeePage", method = RequestMethod.GET)
 	public Page<Employee> getEmployeePage(@QueryParam("page") int page, @QueryParam("size") int size) {
-		return employeeService.getEmployeePage(page, size);
+		return employeeService.getPage(page, size);
 	}
 
 	@RequestMapping(path = "/getTitlePage", method = RequestMethod.GET)
 	public Page<Title> getTitlePage(@QueryParam("page") int page, @QueryParam("size") int size) {
-		return titleService.getTitlePage(page, size);
+		return titleService.getPage(page, size);
+	}
+
+	@RequestMapping(path = "/getSalaryPage", method = RequestMethod.GET)
+	public Page<Salary> getSalaryPage(@QueryParam("page") int page, @QueryParam("size") int size) {
+		return salaryService.getPage(page, size);
+	}
+
+	@RequestMapping(path = "/getDepartmentPage", method = RequestMethod.GET)
+	public Page<Department> getDepartmentPage(@QueryParam("page") int page, @QueryParam("size") int size) {
+		return departmentService.getPage(page, size);
+	}
+
+	@RequestMapping(path = "/getDepartmentEmployeePage", method = RequestMethod.GET)
+	public Page<DepartmentEmployee> getDepartmentEmployeePage(@QueryParam("page") int page,
+			@QueryParam("size") int size) {
+		return departmentEmployeeService.getPage(page, size);
+	}
+
+	@RequestMapping(path = "/getDepartmentManagerPage", method = RequestMethod.GET)
+	public Page<DepartmentManager> getDepartmentManagerPage(@QueryParam("page") int page,
+			@QueryParam("size") int size) {
+		return departmentManagerService.getPage(page, size);
 	}
 
 }
--- src/main/java/com/example/demo/services/impl/DepartmentEmployeeServiceImpl.java
+++ src/main/java/com/example/demo/services/impl/DepartmentEmployeeServiceImpl.java
@@ -3,11 +3,13 @@ package com.example.demo.services.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.entities.DepartmentEmployee;
 import com.example.demo.repositories.DepartmentEmployeeRepository;
 import com.example.demo.services.DepartmentEmployeeService;
 
+@Service
 public class DepartmentEmployeeServiceImpl implements DepartmentEmployeeService {
 
 	@Autowired
