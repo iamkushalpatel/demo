298bc210e5c184d87bb4cf0b43812e41ba083eb3
Kushal Patel <imkushalpatel@live.com>
2019-05-13 15:07:03 +0530

    added services

--- src/main/java/com/example/demo/services/EmployeeService.java
+++ src/main/java/com/example/demo/services/EmployeeService.java
@@ -11,11 +11,11 @@ import com.example.demo.entities.Employee.Gender;
 public interface EmployeeService {
 
 	public Employee addNewEmployee(Employee e);
-	
-	public Employee addNewEmployee(Date birthDate,String firstName,String lastName,Gender gender,Date hireDate);
+
+	public Employee addNewEmployee(Date birthDate, String firstName, String lastName, Gender gender, Date hireDate);
 
 	public List<Employee> getAllEmployee();
-	
-	public Page<Employee> getEmployeePage(int page,int size);
+
+	public Page<Employee> getEmployeePage(int page, int size);
 
 }
